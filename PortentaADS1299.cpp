fbe815b0fff3d1bee237b41b5c059423a2e7e9c8
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2023-09-27 15:15:59 +0530

    This also working Anna

--- PortentaADS1299.cpp
+++ PortentaADS1299.cpp
@@ -1,11 +1,6 @@
 
 
-#define PENA_SCK    9   //PI_1      3
-#define PENA_MISO   10  //PC_2     13
-#define PENA_MOSI   8   //PC_3     11
-#define PENA_CS    7   //PI_0       1
-#define PENA_RESET  1   //PK_1      8
-#define PENA_DRDY   0   //PH_15    15
+
 
 //#include "pins_arduino.h"
 #include "PortentaADS1299.h"
